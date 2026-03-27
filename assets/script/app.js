338124af3f06b40a687db4161d21a74a0e8587d9
Manvir kaur <manvirkaur8193@gmail.com>
2026-03-26 20:06:09 -0500

    added app.js dom element

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+import Subscriber from "./Subscriber.js";
+
+const profileBtn = document.getElementById("profilebtn");
+
+const postForm = document.getElementById("postForm");
+const postText = document.getElementById("postText");
+const postImage = document.getElementById("postImage");
+const postBtn = document.getElementById("postbtn");
+const postsContainer = document.getElementById("postsContainer");
+const fileNameSpan = document.getElementById("fileName");
+const uploadIcon = document.querySelector(".upload-section img");
+
+
+const modal = document.getElementById("modal");
+const closeModalX = document.getElementById("closeModal");
+const modalName = document.getElementById("modalName");
+const modalUsername = document.getElementById("modalUsername");
+const modalEmail = document.getElementById("modalEmail");
+const modalPages = document.getElementById("modalPages");
+const modalGroups = document.getElementById("modalGroups");
+const modalCanMonetize = document.getElementById("modalCanMonetize");
