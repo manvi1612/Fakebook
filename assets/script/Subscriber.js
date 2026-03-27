45a08b062a8c3fcd4fad1bb85912a41b7bb380df
Manvir kaur <manvirkaur8193@gmail.com>
2026-03-26 20:03:53 -0500

    added subscriber.js

--- assets/script/Subscriber.js
+++ assets/script/Subscriber.js
@@ -1,1 +1,31 @@
 'use strict';
+
+
+import User from "./User.js";
+
+export default class Subscriber extends User {
+  #pages;
+  #groups;
+  #canMonetize;
+
+  constructor(id, name, userName, email, pages = [], groups = [], canMonetize = false) {
+    super(id, name, userName, email);
+    this.#pages = pages;
+    this.#groups = groups;
+    this.#canMonetize = canMonetize;
+  }
+
+  get pages() { return this.#pages; }
+  get groups() { return this.#groups; }
+  get canMonetize() { return this.#canMonetize; }
+
+  getInfo() {
+    const baseInfo = super.getInfo();
+    return `
+    ${baseInfo}, 
+    Pages: ${this.#pages.join(", ")}, 
+    Groups: ${this.#groups.join(", ")}, 
+    Can Monetize: ${this.#canMonetize ? "Yes" : "No"}
+    `;
+  }
+}
